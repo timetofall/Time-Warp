cb27519dcc0532abd9ca8a94b168dc11906e7fa9
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-23 12:50:08 -0600

    redo

---
+++ commands/audio/music.js
@@ -0,0 +1,35 @@
+const commando = require('discord.js-commando');
+const YTDL = require('ytdl-core');
+
+class Music extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'play',
+            group: 'audio',
+            memberName: 'play',
+            description: 'Plays a song'
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        message.reply("Successfully joined!");
+                    })
+            }
+        }
+        else
+        {
+            message.reply("You must be a in a voice channel.");
+        }
+    }
+}
+
+module.exports = Music;
