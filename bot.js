8484c38b8cedec6279c110d4f9155518cd86507a
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-23 14:44:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,18 @@ bot.on('ready', function() {
     console.log("Ready");
 })
 
+bot.on('message', message =>{
+    if (message === "!sm") {
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
+    VC.join()
+        .then(connection => {
+            const dispatcher = connection.playFile('AudioFiles/surprise-motherfucker.mp3');
+            dispatcher.on("end", end => {VC.leave()});
+        })
+        .catch(console.error);
+};
+});
+
 bot.login(process.env.BOT_TOKEN); // grabs the token from Heroku Config Vars
