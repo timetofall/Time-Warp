82b92fdc81d4e13baef347518ee877f552b170e9
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-23 11:32:16 -0600

    Update surprise.js

--- commands/audio/surprise.js
+++ commands/audio/surprise.js
@@ -20,13 +20,14 @@ class Surprise extends commando.Command
             {
                 message.member.voiceChannel.join()
                     .then(connection =>{
-                        const dispatcher = connection.playFile('../../AudioFiles/surprise-motherfucker.mp3');
-                        dispatcher.setVolume(0.1);
-                        dispatcher.on('end', () =>
-                        {
-                            // The song has finished
-                            message.guild.voiceConnection.disconnect();
-                        });
+                        // const dispatcher = connection.playFile('../../AudioFiles/surprise-motherfucker.mp3');
+                        // dispatcher.setVolume(0.1);
+                        // dispatcher.on('end', () =>
+                        // {
+                        //     // The song has finished
+                        //     message.guild.voiceConnection.disconnect();
+                        // });
+                        message.reply("Successfully joined!");
                     })
             }
         }
