6f0095277750715858aca1dede89ae47545821e9
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-22 21:13:06 -0600

    Update audio_commands.js

--- audio_commands.js
+++ audio_commands.js
@@ -2,6 +2,7 @@ module.exports = {
         surprise: (message) => {
                 if (message.member.voiceChannel) {
                         message.reply("hi")
+                        message.member.voiceChannel.join()
                 }
                 else {
                         message.reply("ur stoopid")
