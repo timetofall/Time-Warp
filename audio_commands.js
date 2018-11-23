0c3de8303e132e84650975b14b1e00d0e1f779ce
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-22 21:17:28 -0600

    Update audio_commands.js

--- audio_commands.js
+++ audio_commands.js
@@ -2,7 +2,9 @@ module.exports = {
         surprise: (message) => {
                 if (message.member.voiceChannel) {
                         message.reply("bye")
-                        message.member.voiceChannel.join()
+                        message.member.voiceChannel.join().then(connection => {
+                                message.reply('I have successfully connected to the channel!');
+                        })
                 }
                 else {
                         message.reply("ur stoopid")
