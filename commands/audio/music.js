cad9257a6faa40683e7d4e92ffe94bea8eb9b1c1
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-23 13:10:29 -0600

    Update music.js

--- commands/audio/music.js
+++ commands/audio/music.js
@@ -1,6 +1,17 @@
 const commando = require('discord.js-commando');
 // const YTDL = require('ytdl-core');
 
+function Play(connection, message)
+{
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playFile(server.queue[0])
+    message.reply(server.queue[0])
+    server.queue.shift();
+    server.dispatcher.on("end", function(){
+        connection.disconnect()
+    })
+}
+
 class Music extends commando.Command
 {
     constructor(client)
@@ -19,9 +30,16 @@ class Music extends commando.Command
         {
             if(!message.guild.voiceConnection)
             {
+                if(!servers[message.guild.id])
+                {
+                    servers[message.guild.id] = {queue: []}
+                }
                 message.member.voiceChannel.join()
                     .then(connection =>{
+                        var server = servers[message.guild.id];
                         message.reply("Successfully join");
+                        server.queue.push('../../AudioFiles/surprise-motherfucker.mp3');
+                        Play(connection, message);
                     })
             }
         }
