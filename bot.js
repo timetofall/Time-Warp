d893e04b313830edf80803dacea90d9354fec3c4
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-22 22:23:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,31 +38,6 @@ function processCommand(receivedMessage) {
 }
 
 
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) {
-     message.reply("nope")
-     return;
-  }
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    message.reply("nope")
-    if (message.member.voiceChannel) {
-      message.reply("yes")
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
-
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
