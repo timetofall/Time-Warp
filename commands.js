fda38a6ec60cdb71d37cf66ce68bf49af12b7a70
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-22 20:17:49 -0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,8 @@
 module.exports = {
         popcorns: (receivedMessage) => {
             receivedMessage.channel.send("James is fat, <:popfeels:477323348125286405>")
-        }
+        },
+        helpCommand: (receivedMessage) => {
+            receivedMessage.channel.send("Git gud scrub")
+        },
     };
