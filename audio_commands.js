ac7340874405692d12b1008303f19d897a4ee19c
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-22 20:31:16 -0600

    Update audio_commands.js

--- audio_commands.js
+++ audio_commands.js
@@ -1,4 +1,8 @@
 module.exports = {
         surprise: (message) => {
+                message.member.voiceChannel.join()
+                        .then(connection => {
+                                message.reply('I have successfully connected to the channel!');
+                })
         },
 };
