b82cd8ac254f9add02fa08900399d910bd45d86d
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-23 11:42:22 -0600

    Create surprise.js

---
+++ commands/audio/surprise.js
@@ -0,0 +1,36 @@
+const commando = require('discord.js-commando');
+
+class Surprise extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'join',
+            group: 'audio',
+            memberName: 'join',
+            description: 'Joins the voice channel of the commander'
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        message.reply("Successfully joined!");
+                        message.guild.voiceConnection.disconnect();
+                        message.reply("Successfully left!");
+                    })
+            }
+        }
+        else
+        {
+            message.reply("You must be a in a voice channel.");
+        }
+    }
+}
+
+module.exports = Surprise;
