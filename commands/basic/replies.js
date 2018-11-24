09e3a97d8616bd118d6184a99d94f313af4c055b
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-24 14:54:49 -0600

    lol

--- commands/basic/replies.js
+++ commands/basic/replies.js
@@ -18,16 +18,6 @@ module.exports = {
     },
 };
 
-// function james(){
-//     let myArray = ['james is fat', 'james is ugly', 'james is PHAT'];
-//     return myArray[Math.floor(Math.random() * myArray.length)];
-// }
-//
-// function wes(){
-//     let myArray = ["hi wes", "hi thug"];
-//     return myArray[Math.floor(Math.random() * myArray.length)];
-// }
-
 function test_names(message, names){
     return names.find(name => message.indexOf(name) !== -1);
 }
