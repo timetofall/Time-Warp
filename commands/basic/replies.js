078b125e08a316c967f4debcfdb91476fc78e229
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-24 00:21:41 -0600

    Update replies.js

--- commands/basic/replies.js
+++ commands/basic/replies.js
@@ -1,15 +1,6 @@
 module.exports = {
     process: (message) => {
-        // let message_lower = message.content.toLowerCase();
-        // let message_list = message_lower.split(" ");
-        //
-        // if(message_list.includes("james"))
-        // {
-        //     return james();
-        // }
-
         let message_string = message.content.toLowerCase();
-        message.channel.send(message_string.indexOf("james"));
         if(message_string.indexOf("james") !== -1)
         {
             return james();
