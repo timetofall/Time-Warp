0850bbdd5b349059c15321fb33a5f002d992f37b
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-24 17:26:07 -0600

    Update surprise_local.js

--- commands/audio/surprise_local.js
+++ commands/audio/surprise_local.js
@@ -5,7 +5,7 @@ function Play(connection, message)
 {
     let server = play_queue[message.guild.id];
     const streamOptions = { seek: 0, volume: .25 };
-    server.dispatcher = connection.play(server.queue[0], streamOptions);
+    server.dispatcher = connection.playFile(server.queue[0], streamOptions);
     server.queue.shift();
     server.dispatcher.on("end", function(){
         if(server.queue[0])
