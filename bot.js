5168b699bd22bd9f2937150371fba0934befb110
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-24 14:27:39 -0600

    audio

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ bot.registry.registerGroup('audio', 'Audio');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
+global.servers = {}
+
 bot.on('ready', function() {
     console.log("Time-Warp Ready");
     bot.user.setActivity('Diablo Immortal', { type: 'PLAYING' });
--- commands/audio/music.js
+++ commands/audio/music.js
@@ -1,60 +1,60 @@
 const commando = require('discord.js-commando');
 const YTDL = require('ytdl-core');
 
-// function Play(connection, message)
-// {
-//     var server = servers[message.guild.id];
-//     // message.reply(server.queue[0])
-//     // server.dispatcher = connection.playFile(server.queue[0])
-//     server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-//     server.queue.shift();
-//     server.dispatcher.on("end", function(){
-//         if(server.queue[0])
-//         {
-//             Play(connection, message);
-//         }
-//         else
-//         {
-//             connection.disconnect();
-//         }
-//     })
-// }
+function Play(connection, message)
+{
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function(){
+        if(server.queue[0])
+        {
+            Play(connection, message);
+        }
+        else
+        {
+            connection.disconnect();
+        }
+    })
+}
 
-// class Music extends commando.Command
-// {
-//     constructor(client)
-//     {
-//         super(client,{
-//             name: 'play',
-//             group: 'audio',
-//             memberName: 'play',
-//             description: 'Plays a song'
-//         });
-//     }
-//
-//     async run(message, args)
-//     {
-//         if(message.member.voiceChannel)
-//         {
-//             if(!message.guild.voiceConnection)
-//             {
-//                 if(!servers[message.guild.id])
-//                 {
-//                     servers[message.guild.id] = {queue: []}
-//                 }
-//                 message.member.voiceChannel.join()
-//                     .then(connection =>{
-//                         var server = servers[message.guild.id];
-//                         message.reply("Successfully joined");
-//                     })
-//                     .catch(console.log);
-//             }
-//         }
-//         else
-//         {
-//             message.reply("You must be a in a voice channel.");
-//         }
-//     }
-// }
-//
-// module.exports = Music;
+class Music extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'play',
+            group: 'audio',
+            memberName: 'play',
+            description: 'Plays a song'
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                if(!servers[message.guild.id])
+                {
+                    servers[message.guild.id] = {queue: []}
+                }
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        var server = servers[message.guild.id];
+                        message.reply("Successfully Joined!");
+                        server.queue.push(args);
+                        Play(connection, message);
+                    })
+                    .catch(console.log);
+            }
+        }
+        else
+        {
+            message.reply("You must be a in a voice channel.");
+        }
+    }
+}
+
+module.exports = Music;
