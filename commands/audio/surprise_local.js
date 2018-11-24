945005387e6f127fedd576e35f8f45785bcf7f51
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-24 15:39:10 -0600

    Create surprise_local.js

---
+++ commands/audio/surprise_local.js
@@ -0,0 +1,59 @@
+const commando = require('discord.js-commando');
+
+function Play(connection, message)
+{
+    let server = play_queue[message.guild.id];
+    server.dispatcher = connection.playFile('../../surprise-motherfucker.mp3');
+    server.queue.shift();
+    server.dispatcher.on("end", function(){
+        if(server.queue[0])
+        {
+            Play(connection, message);
+        }
+        else
+        {
+            connection.disconnect();
+        }
+    })
+}
+
+class Surprise_Local extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'sm1',
+            group: 'audio',
+            memberName: 'sm1',
+            description: 'Plays a song'
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                if(!play_queue[message.guild.id])
+                {
+                    play_queue[message.guild.id] = {queue: []}
+                }
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        let server = play_queue[message.guild.id];
+                        message.reply("Successfully Joined!");
+                        server.queue.push('../../surprise-motherfucker.mp3');
+                        Play(connection, message);
+                    })
+                    .catch(console.log);
+            }
+        }
+        else
+        {
+            message.reply("You must be a in a voice channel.");
+        }
+    }
+}
+
+module.exports = Surprise_Local;
