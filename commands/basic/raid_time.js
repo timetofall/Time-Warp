d6a0d7ad258b65452216949169ea131bf36038c1
timetofall <retsium@gmail.com>
2018-11-25 17:55:18 -0500

    Update raid_time.js
    made it 15 minutes instead of 10

--- commands/basic/raid_time.js
+++ commands/basic/raid_time.js
@@ -9,15 +9,15 @@ module.exports = {
             let day_hour = date.getHours();
             let day_minute = date.getMinutes();
             let day_second = date.getSeconds();
-            let raid_tuesday = channel && day_number === 2 && day_hour === 21 && day_minute === 49; //&& day_second === 00; //if minute is at 39 it will do it at 40
-            let raid_wednesday = channel && day_number === 3 && day_hour === 21 && day_minute === 49;
-            let raid_sunday = channel && day_number === 0 && day_hour === 20 && day_minute === 49;
+            let raid_tuesday = channel && day_number === 2 && day_hour === 21 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
+            let raid_wednesday = channel && day_number === 3 && day_hour === 21 && day_minute === 44;
+            let raid_sunday = channel && day_number === 0 && day_hour === 20 && day_minute === 44;
 
             //let raid_test = channel && day_number === 0 && day_hour === 17 && day_minute === 28;
 
             if(raid_tuesday || raid_wednesday || raid_sunday)
             {
-                channel.send(`${val}, Raid starts in 10 minutes!`)
+                channel.send(`${val}, Raid starts in 15 minutes!`)
             }
             else
             {
