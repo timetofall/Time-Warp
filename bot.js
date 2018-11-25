f99001d22b693f147f0003a48416f75078dcbaa6
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-25 17:07:48 -0600

    remove raid time

--- bot.js
+++ bot.js
@@ -32,7 +32,6 @@ bot.on('ready', function() {
 bot.on('message', (message) => {
     const cmds = require('./commands/basic');
 
-    cmds.raid_time.raid_toggle(message);
     if (message.author !== bot.user) {
         let val = cmds.replies.process(message);
         if (val){
