6759eca69958c744c9fe6dc1637b2880fee7bbfc
Chris Tran <chris.l.tran.2016@gmail.com>
2018-11-27 09:41:21 -0600

    fixed modular

--- commands/basic/index.js
+++ commands/basic/index.js
@@ -1,3 +1,3 @@
 exports.replies = require("./replies.js");
-exports.raid_time = require("./raid.js");
+exports.raid = require("./raid.js");
 exports.intervals = require("./intervals.js");
--- commands/basic/intervals.js
+++ commands/basic/intervals.js
@@ -1,5 +1,6 @@
 function status(bot)
 {
+    bot.user.setActivity('Starting Up');
     setInterval(() => {
         let date = new Date();
         let day_number = date.getDay();
@@ -8,21 +9,26 @@ function status(bot)
         let day_second = date.getSeconds();
         
         //Day & Hour is in GMT Time
-        let raid_tuesday = channel && day_number === 3 && day_hour === 1 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
-        let raid_wednesday = channel && day_number === 4 && day_hour === 1 && day_minute === 44;
-        let raid_sunday = channel && day_number === 1 && day_hour === 0 && day_minute === 44;
+        let raid_tuesday = day_number === 3 && day_hour === 1 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
+        let raid_wednesday = day_number === 4 && day_hour === 1 && day_minute === 44;
+        let raid_sunday = day_number === 1 && day_hour === 0 && day_minute === 44;
 
         if(raid_tuesday || raid_wednesday || raid_sunday)
         {
-            if (raid_on){
+            if (raid_on)
+            {
                 bot.user.setActivity('Salt Raid', { type: 'WATCHING' });
+            }
+            else
+            {
+                bot.user.setActivity('Diablo Immortal', {type: 'PLAYING'});
             }
         }
         else
         {
             bot.user.setActivity('Diablo Immortal', {type: 'PLAYING'});
         }
-    }, 60000);
+    }, 60 * 1000);
 }
 
 exports.status = status;
