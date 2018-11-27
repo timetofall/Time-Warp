f823308114925185555f4ff5ccc898f6efc60f6e
timetofall <retsium@gmail.com>
2018-11-26 19:43:34 -0500

    TESTING 5

--- commands/basic/raid_time.js
+++ commands/basic/raid_time.js
@@ -8,11 +8,11 @@ module.exports = {
             let day_hour = date.getHours();
             let day_minute = date.getMinutes();
             let day_second = date.getSeconds();
-            let raid_tuesday = channel && day_number === 2 && day_hour === 20 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
-            let raid_wednesday = channel && day_number === 3 && day_hour === 20 && day_minute === 44;
-            let raid_sunday = channel && day_number === 0 && day_hour === 19 && day_minute === 44;
+            let raid_tuesday = channel && day_number === 3 && day_hour === 1 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
+            let raid_wednesday = channel && day_number === 4 && day_hour === 1 && day_minute === 44;
+            let raid_sunday = channel && day_number === 1 && day_hour === 0 && day_minute === 44;
 
-            let raid_test = channel && day_number === 2 && day_hour === 23; // && day_minute === 28;
+            let raid_test = channel && day_number === 2 && day_hour === 0; // && day_minute === 28;
 
             if (raid_on){
                 if(raid_tuesday || raid_wednesday || raid_sunday || raid_test)
