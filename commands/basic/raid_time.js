e0653b0307fe10c7a7723a466409654e3abe6e1f
timetofall <retsium@gmail.com>
2018-11-26 19:24:59 -0500

    Testing timezone stuff

--- commands/basic/raid_time.js
+++ commands/basic/raid_time.js
@@ -1,7 +1,7 @@
 module.exports = {
     raid_time: (bot) => {
-        let val = bot.guilds.find(val => val == "SaltGuild").roles.find(val => val.name == "@raiders");
-        const channel = bot.channels.find(val => val.name === 'raid');
+        let val = bot.guilds.find(val => val == "SQUAD").roles.find(val => val.name == "What");
+        const channel = bot.channels.find(val => val.name === 'squad');
         setInterval(() => {
             let date = new Date();
             let day_number = date.getDay();
@@ -12,12 +12,13 @@ module.exports = {
             let raid_wednesday = channel && day_number === 3 && day_hour === 20 && day_minute === 44;
             let raid_sunday = channel && day_number === 0 && day_hour === 19 && day_minute === 44;
 
-            //let raid_test = channel && day_number === 0 && day_hour === 17 && day_minute === 28;
+            let raid_test = channel && day_number === 2 && day_hour === 23; // && day_minute === 28;
 
             if (raid_on){
-                if(raid_tuesday || raid_wednesday || raid_sunday)
+                if(raid_tuesday || raid_wednesday || raid_sunday || raid_test)
                     {
-                        channel.send(`${val}, Raid starts in 15 minutes!`)
+                        //channel.send(`${val}, Raid starts in 15 minutes!`)
+                        console.log("ITS WORKIN BROTHER", raid_on);
                     }
             }
         }, 60000); // Runs this every 1 minute.
