8fff69af499205c524683c07db383931c9b27947
timetofall <retsium@gmail.com>
2018-12-02 20:58:52 -0500

    changed a line for zathik()

--- commands/basic/replies_functions.js
+++ commands/basic/replies_functions.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     zathik(){
-        let myArray = ["No matter what you do Chris, you still wont be an officer", "Learn to use Fortifying Brew, Chris"];
+        let myArray = ["Did you hear that Bob is an officer now? Y I K E S", "Learn to use Fortifying Brew, Chris"];
         return myArray[Math.floor(Math.random() * myArray.length)];
     },
 
