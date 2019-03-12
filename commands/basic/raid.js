bffec12667f3d4dbed132fc90b7eae5d975bbe3d
MeisterJames <46969223+MeisterJames@users.noreply.github.com>
2019-03-11 21:58:05 -0400

    Update raid.js

--- commands/basic/raid.js
+++ commands/basic/raid.js
@@ -93,10 +93,10 @@ module.exports = {
             //let day_second = date.getSeconds();
             
             //Day & Hour is in GMT Time
-            let raid_monday = channel && day_number === 2 && day_hour === 1 && day_minute === 44;
-            let raid_tuesday = channel && day_number === 3 && day_hour === 1 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
-            let raid_wednesday = channel && day_number === 4 && day_hour === 1 && day_minute === 44;
-            let raid_sunday = channel && day_number === 1 && day_hour === 0 && day_minute === 44;
+            let raid_monday = channel && day_number === 2 && day_hour === 0 && day_minute === 44;
+            let raid_tuesday = channel && day_number === 3 && day_hour === 0 && day_minute === 44; //&& day_second === 00; //if minute is at 39 it will do it at 40
+            let raid_wednesday = channel && day_number === 4 && day_hour === 0 && day_minute === 44;
+            let raid_sunday = channel && day_number === 1 && day_hour === 23 && day_minute === 44;
 
             // Opens the data from the DB and if raidStatuses.raid_status is true, it will do the if statements inside)
             raidStatus.findOne(function (err, raidStatuses) {
